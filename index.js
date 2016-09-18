6ee31c6fffddbcb737f09ba05a385a27d076d96f
cordelia273 <cordelia273@naver.com>
2016-09-18 21:03:36 +0900

    1234

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-document.querySelector("#vibrate-one-second").addEventListener("click", function () {
-navigator.mozVibrate(1000);
-}, false);
-
-document.querySelector("#vibrate-twice").addEventListener("click", function () {
-navigator.mozVibrate([200, 100, 200, 100]);
-}, false);
-
-
-document.querySelector("#vibrate-long-time").addEventListener("click", function () {
-navigator.mozVibrate(5000);
-}, false);
-
-document.querySelector("#vibrate-off").addEventListener("click", function () {
- navigator.mozVibrate(0);
-}, false);
+$(document).ready(function(){
+  $('#al').click(function(){
+    alert("asdfasdf");
+  });
+});
