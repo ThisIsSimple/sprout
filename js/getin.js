61af305e35c1cdf70a216fce213111c3bdbbbf40
cordelia273 <cordelia273@naver.com>
2016-09-22 00:42:05 +0900

    2016/09/21
    survey

--- js/getin.js
+++ js/getin.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+
   $('.close').click(function(){
-    $('.sometext').slideUp();
+    $(this).parent().parent().parent().slideUp();
+  });
+
+  //설문조사
+  $('.btn').click(function(){
+    $(this).parent().parent().parent().slideUp();
+    $('#survey').delay(700).slideDown();
+  });
+
+  //설문조사 완료
+  $('#survey .btn-large').click(function(){
+    $(this).parent().parent().parent().slideUp();
+    $('#thank').delay(1000).slideDown();
   });
 });
