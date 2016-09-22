3bf48ac7bf03f3f26eec6938964908da9502e0a1
cordelia273 <cordelia273@naver.com>
2016-09-22 20:39:45 +0900

    2016/09/22

--- js/admin.js
+++ js/admin.js
@@ -3,15 +3,8 @@ $(document).ready(function(){
     $('#sidebar a').removeClass('active');
     $(this).addClass('active');
   });
-  ('.tooltipped').tooltip({delay: 10, position: top});
-  $(function() {
 
-  $("#bars li .bar").each( function( key, bar ) {
-    var number = $(this).data('number');
+  ('.tooltipped').tooltip({});
 
-    $(this).animate({
-      'height' : number*10 + 'px'
-    }, 1000);
-  });
-});
+  $('ul.tabs').tabs();
 })
